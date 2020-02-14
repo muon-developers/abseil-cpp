9de20bd0273a5ab00f6bd74e3c3c9bdb96b5ccda
ABHISHEK BOSE <abose@ABHISHEKs-MacBook-Pro.local>
2020-02-14 13:34:52 +0000

    remove one test

--- absl/algorithm/algorithm_test.cc
+++ absl/algorithm/algorithm_test.cc
@@ -186,14 +186,6 @@ TEST_F(LinearSearchTest, linear_search_1) {
   EXPECT_FALSE(absl::linear_search_1(container_.begin(), container_.end(), 4));
 }
 
-TEST_F(LinearSearchTest, linear_searchConst) {
-  const std::vector<int> *const const_container = &container_;
-  EXPECT_TRUE(
-      absl::linear_search_1(const_container->begin(), const_container->end(), 3));
-  EXPECT_FALSE(
-      absl::linear_search_1(const_container->begin(), const_container->end(), 4));
-}
-
 //new tests add - end
 
 }  // namespace
